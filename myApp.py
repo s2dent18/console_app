7edbb39394c0a3f0a64a9f54d3f3ea01c548232e
Komardin Aleksandr <s2dent18@yandex.ru>
2023-07-31 00:05:35 +0300

    Add 3rd and 4th commands

--- myApp.py
+++ myApp.py
@@ -1,7 +1,12 @@
 import psycopg2
 import sys
+import random
+import time
 from psycopg2 import Error
+from faker import Faker
+from transliterate import translit
 from settings import database_conf
+from base import second_name_base
 
 
 try:
@@ -22,6 +27,9 @@ def main():
         auto_filling()
     elif sys.argv[1] == '5':
         filtered_output()
+    cursor.close()
+    connection.close()
+    print("Соединение с PostgreSQL закрыто")
 
 def create_table():
     try:
@@ -36,10 +44,7 @@ def create_table():
         connection.commit()
         print(f"Таблица people успешно создана")
     except (Exception, Error) as err:
-        print("Ошибка при работе с PostgreSQL", err)
-    cursor.close()
-    connection.close()
-    print("Соединение с PostgreSQL закрыто")
+        print(err)
 
 def add_note(*args):
     try:
@@ -53,19 +58,64 @@ def add_note(*args):
         connection.commit()
         print("Запись добавлена")
     except (Exception, Error) as err:
-        print("Ошибка при работе с PostgreSQL", err)
-    cursor.close()
-    connection.close()
-    print("Соединение с PostgreSQL закрыто")
+        print(err)
 
 def unique_output():
-    pass
+    try:
+        cursor.execute('''
+        SELECT DISTINCT ON (fio, birthdate) fio, TO_CHAR (birthdate, 'YYYY-MM-DD'), gender, EXTRACT (YEAR FROM AGE(birthdate))::INT FROM people ORDER BY fio;
+        ''')
+        result = cursor.fetchall()
+        if result:
+            for person in result:
+                print(person)
+        else:
+            print('пусто')
+    except (Exception, Error) as err:
+        print(err)
 
 def auto_filling():
-    pass
+    fake = Faker('ru_RU')
+    hundred_data = []
+    data = []
+    lenght = 1000000
+    start_time = time.time()
+    print('Генерация 1000000 случайных записей ')
+    printProgressBar(0, lenght, prefix = 'Прогресс:', suffix = 'Выполнено', length = 50)
+    for _ in range(lenght):
+        gender = random.choice(['M', 'F'])
+        if gender == 'M':
+            fio = " ".join([fake.last_name_male(), fake.first_name_male(), fake.middle_name_male()])
+        else:
+            fio = " ".join([fake.last_name_female(), fake.first_name_female(), fake.middle_name_female()])
+        out = (translit(fio, language_code='ru', reversed=True), fake.date_of_birth(), gender)
+        data.append(out)
+        printProgressBar(_ + 1, lenght, prefix = 'Прогресс:', suffix = 'Выполнено', length = 50)
+    print('')
+    print('Выполняется загрузка в базу данных')
+    cursor.executemany('''
+    INSERT INTO people (fio, birthdate, gender) VALUES (%s, %s, %s);
+    ''', data)
+    for _ in range(100):
+        fio = " ".join([random.choice(second_name_base), fake.first_name_male(), fake.middle_name_male()])
+        out = (translit(fio, language_code='ru', reversed=True), fake.date_of_birth(), 'M')
+        hundred_data.append(out)
+    print("Сгенерировано 100 случайных записей мужского пола с фамилией, начинающейся с 'F'")
+    cursor.executemany('''
+    INSERT INTO people (fio, birthdate, gender) VALUES (%s, %s, %s);
+    ''', hundred_data)
+    end_time = time.time()
+    connection.commit()
+    print(f"Время выполнения запроса: {end_time - start_time:.2f} секунд")
 
 def filtered_output():
     pass
+
+def printProgressBar(iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
 
 if __name__ == '__main__':
     if len(sys.argv) < 1:
